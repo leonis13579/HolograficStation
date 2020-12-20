5e8bd55cdf0ff793ab7ff62e67d75668e642782b
Leo <leonis13579@gmail.com>
2020-12-20 14:55:01 +0300

    Created demo with StateControll

---
+++ Assets/Scripts/ChangeObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour
+{
+    private WebConnection _states;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _states = GameObject.FindGameObjectWithTag("States").GetComponent<WebConnection>();
+
+        _states.ConnectToDelegate("Show: Cube", () => { changeObject("Cube"); });
+        _states.ConnectToDelegate("Show: Sphere", () => { changeObject("Sphere"); });
+        _states.ConnectToDelegate("Show: Capsule", () => { changeObject("Capsule"); });
+        _states.ConnectToDelegate("Show: Cylinder", () => { changeObject("Cylinder"); });
+    }
+
+    void changeObject(string gameObjectName)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(transform.GetChild(i).gameObject.name.Equals(gameObjectName));
+        }
+    }
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float RotationStep;
+    float rotation_speed = 0;
+
+    private WebConnection _states;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _states = GameObject.FindGameObjectWithTag("States").GetComponent<WebConnection>();
+
+        _states.ConnectToDelegate("Add rotation speed", () => { rotation_speed += RotationStep; Debug.Log("Added to " + rotation_speed); });
+        _states.ConnectToDelegate("Sub rotation speed", () => { rotation_speed -= RotationStep; Debug.Log("Added to " + rotation_speed); });
+        _states.ConnectToDelegate("Stop rotation", () => { rotation_speed = 0; Debug.Log("Canceled rotation"); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotation.y = rotation.y < 360 ? rotation.y + rotation_speed : 0;
+        Quaternion quaternion = new Quaternion();
+        quaternion.eulerAngles = rotation;
+        transform.rotation = quaternion;
+    }
+}
